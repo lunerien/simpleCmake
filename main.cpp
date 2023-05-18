e08e42f2ef5859053bf71955c7ca60bb8e40693b
lukasz.kala <lukasz.kala@techsterowniki.pl>
2023-05-18 15:04:56 +0200

    public include

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-// #include "Klasa.h"
-#include "klasy/Klasa2.h"
+#include "Klasa.h"
+// #include "klasy/Klasa2.h"
 
 
 
 int main(int ac, char** av)
 {
     std::cout << "test" << "\n";
-    // Klasa kl;
-    Klasa2 kl2;
+    Klasa kl;
+    // volatile Klasa2 kl2;
 
     return 0;
 
